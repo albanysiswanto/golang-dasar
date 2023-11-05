cafccc2d38c2c783954b6a89238f571a2e50b099
Albany Siswanto <albanysiswantoo@gmail.com>
2023-11-05 08:48:47 +0700

    package strings

--- helper/access-modifier.go
+++ helper/access-modifier.go
@@ -1,4 +1,4 @@
 package helper
 
-var version = "1.0.0"                    //Tidak bisa dapat di akses keluar
+// var version = "1.0.0"                    //Tidak bisa dapat di akses keluar
 var Application = "Belajar Golang Dasar" // Bisa di akses keluar package
